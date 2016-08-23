90d6307d7f261e146e4841e3d617e6d7e1481c0e
rockgarden <ro>
2016-08-23 11:45:06 +0800

    init

---
+++ SimpleLib/SimpleLib.h
@@ -0,0 +1,19 @@
+//
+//  SimpleLib.h
+//  SimpleLib
+//
+//  Created by wangkan on 16/8/23.
+//  Copyright © 2016年 rockgarden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleLib.
+FOUNDATION_EXPORT double SimpleLibVersionNumber;
+
+//! Project version string for SimpleLib.
+FOUNDATION_EXPORT const unsigned char SimpleLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleLib/PublicHeader.h>
+
+
