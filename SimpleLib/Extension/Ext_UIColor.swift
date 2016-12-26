76d3b8f1795aaf338412d8c0ad44ce73a80851d5
rockgarden <rockgarden@sina.com>
2016-12-26 22:40:06 +0800

    opt

--- SimpleLib/Extension/Ext_UIColor.swift
+++ SimpleLib/Extension/Ext_UIColor.swift
@@ -24,15 +24,31 @@ extension UIColor {
     }
     
     /// EZSE: init method with hex string and alpha(default: 1)
-    public convenience init?(hexString: String, alpha: CGFloat = 1.0) {
-        var formatted = hexString.replacingOccurrences(of: "0x", with: "")
+    public convenience init?(_ hexStr: String, alpha: CGFloat = 1.0) {
+        var formatted = hexStr.replacingOccurrences(of: "0x", with: "")
         formatted = formatted.replacingOccurrences(of: "#", with: "")
         if let hex = Int(formatted, radix: 16) {
             let red = CGFloat(CGFloat((hex & 0xFF0000) >> 16) / 255.0)
             let green = CGFloat(CGFloat((hex & 0x00FF00) >> 8) / 255.0)
             let blue = CGFloat(CGFloat((hex & 0x0000FF) >> 0) / 255.0)
-            self.init(red: red, green: green, blue: blue, alpha: alpha) } else {
+            self.init(red: red, green: green, blue: blue, alpha: alpha) }
+        else {
             return nil
+        }
+    }
+
+    class func hex (_ hexStr : NSString, alpha : CGFloat) -> UIColor {
+        let realHexStr = hexStr.replacingOccurrences(of: "#", with: "")
+        let scanner = Scanner(string: realHexStr as String)
+        var color: UInt32 = 0
+        if scanner.scanHexInt32(&color) {
+            let r = CGFloat((color & 0xFF0000) >> 16) / 255.0
+            let g = CGFloat((color & 0x00FF00) >> 8) / 255.0
+            let b = CGFloat(color & 0x0000FF) / 255.0
+            return UIColor(red:r,green:g,blue:b,alpha:alpha)
+        } else {
+            print("invalid hex string", terminator: "")
+            return UIColor.white
         }
     }
     
