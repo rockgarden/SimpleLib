3cd424bad78e83636c519933571a4c3d43b144a7
rockgarden <rockgarden@sina.com>
2017-03-01 22:00:38 +0800

    add

--- SimpleLib/Block/BlockWebView.swift
+++ SimpleLib/Block/BlockWebView.swift
@@ -10,43 +10,42 @@ import UIKit
 
 #if os(iOS)
 
-///Make sure you use  "[weak self] (NSURLRequest) in" if you are using the keyword self inside the closure or there might be a memory leak
-public class BlockWebView: UIWebView, UIWebViewDelegate {
-    public var didStartLoad: ((NSURLRequest) -> ())?
-    public var didFinishLoad: ((NSURLRequest) -> ())?
-    public var didFailLoad: ((NSURLRequest, NSError) -> ())?
+    ///Make sure you use  `[weak self] (NSURLRequest) in` if you are using the keyword `self` inside the closure or there might be a memory leak
+    open class BlockWebView: UIWebView, UIWebViewDelegate {
+        open var didStartLoad: ((URLRequest) -> Void)?
+        open var didFinishLoad: ((URLRequest) -> Void)?
+        open var didFailLoad: ((URLRequest, Error) -> Void)?
 
-    public var shouldStartLoadingRequest: ((NSURLRequest) -> (Bool))?
+        open var shouldStartLoadingRequest: ((URLRequest) -> (Bool))?
 
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        delegate = self
-    }
+        public override init(frame: CGRect) {
+            super.init(frame: frame)
+            delegate = self
+        }
 
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
+        public required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+        }
 
+        open func webViewDidStartLoad(_ webView: UIWebView) {
+            didStartLoad? (webView.request!)
+        }
 
-    public func webViewDidStartLoad(webView: UIWebView) {
-        didStartLoad? (webView.request!)
-    }
+        open func webViewDidFinishLoad(_ webView: UIWebView) {
+            didFinishLoad? (webView.request!)
+        }
 
-    public func webViewDidFinishLoad(webView: UIWebView) {
-        didFinishLoad? (webView.request!)
-    }
+        open func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+            didFailLoad? (webView.request!, error)
+        }
 
-    public func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
-        didFailLoad? (webView.request!, error!)
-    }
-
-    public func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        if let should = shouldStartLoadingRequest {
-            return should (request)
-        } else {
-            return true
+        open func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+            if let should = shouldStartLoadingRequest {
+                return should (request)
+            } else {
+                return true
+            }
         }
     }
-}
-
+    
 #endif
--- SimpleLib/Extension/Ext_UIImageView.swift
+++ SimpleLib/Extension/Ext_UIImageView.swift
@@ -1,9 +1,9 @@
 //
-//  Ext_UIImageView.swift
-//  SimpleLib
+//  UIImageViewExtensions.swift
+//  EZSwiftExtensions
 //
-//  Created by wangkan on 16/9/15.
-//  Copyright © 2016年 rockgarden. All rights reserved.
+//  Created by Goktug Yilmaz on 15/07/15.
+//  Copyright (c) 2015 Goktug Yilmaz. All rights reserved.
 //
 
 import UIKit
@@ -14,7 +14,7 @@ extension UIImageView {
         self.init(frame: CGRect(x: x, y: y, width: w, height: h))
         image = UIImage(named: imageName)
     }
-    
+
     /// EZSwiftExtensions
     public convenience init(x: CGFloat, y: CGFloat, imageName: String, scaleToWidth: CGFloat) {
         self.init(frame: CGRect(x: x, y: y, width: 0, height: 0))
@@ -25,20 +25,20 @@ extension UIImageView {
             assertionFailure("EZSwiftExtensions Error: The imageName: '\(imageName)' is invalid!!!")
         }
     }
-    
+
     /// EZSwiftExtensions
     public convenience init(x: CGFloat, y: CGFloat, w: CGFloat, h: CGFloat, image: UIImage) {
         self.init(frame: CGRect(x: x, y: y, width: w, height: h))
         self.image = image
     }
-    
+
     /// EZSwiftExtensions
     public convenience init(x: CGFloat, y: CGFloat, image: UIImage, scaleToWidth: CGFloat) {
         self.init(frame: CGRect(x: x, y: y, width: 0, height: 0))
         self.image = image
         scaleImageFrameToWidth(width: scaleToWidth)
     }
-    
+
     /// EZSwiftExtensions, scales this ImageView size to fit the given width
     public func scaleImageFrameToWidth(width: CGFloat) {
         guard let image = image else {
@@ -50,7 +50,7 @@ extension UIImageView {
         let newHeigth = image.size.height / widthRatio
         frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: newWidth, height: newHeigth)
     }
-    
+
     /// EZSwiftExtensions, scales this ImageView size to fit the given height
     public func scaleImageFrameToHeight(height: CGFloat) {
         guard let image = image else {
@@ -62,39 +62,68 @@ extension UIImageView {
         let newWidth = image.size.width / heightRatio
         frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: newWidth, height: newHeight)
     }
-    
+
     /// EZSwiftExtensions
     public func roundSquareImage() {
         self.clipsToBounds = true
         self.layer.cornerRadius = self.frame.size.width / 2
     }
-    
+
+    /// EZSE: Initializes an UIImage from URL and adds into current ImageView
+    public func image(url: String) {
+        
+        ez.requestImage(url, success: { (image) -> Void in
+            if let img = image {
+                DispatchQueue.main.async {
+                    self.image = img
+                }
+            }
+        })
+    }
+
+    /// EZSE: Initializes an UIImage from URL and adds into current ImageView with placeholder
+    public func image(url: String, placeholder: UIImage) {
+        self.image = placeholder
+        image(url: url)
+    }
+
+    /// EZSE: Initializes an UIImage from URL and adds into current ImageView with placeholder
+    public func image(url: String, placeholderNamed: String) {
+        if let image = UIImage(named: placeholderNamed) {
+            self.image(url: url, placeholder: image)
+        } else {
+            image(url: url)
+        }
+    }
+
+    // MARK: Deprecated 1.8
+
     /// EZSwiftExtensions
+    @available(*, deprecated: 1.8, renamed: "image(url:)")
     public func imageWithUrl(url: String) {
         ez.requestImage(url, success: { (image) -> Void in
             if let img = image {
-                self.image = img
+                DispatchQueue.main.async {
+                    self.image = img
+                }
             }
         })
     }
-    
+
     /// EZSwiftExtensions
+    @available(*, deprecated: 1.8, renamed: "image(url:placeholder:)")
     public func imageWithUrl(url: String, placeholder: UIImage) {
         self.image = placeholder
-        ez.requestImage(url, success: { (image) -> Void in
-            if let img = image {
-                self.image = img
-            }
-        })
+        imageWithUrl(url: url)
     }
-    
+
     /// EZSwiftExtensions
+    @available(*, deprecated: 1.8, renamed: "image(url:placeholderNamed:)")
     public func imageWithUrl(url: String, placeholderNamed: String) {
-        self.image = UIImage(named: placeholderNamed)
-        ez.requestImage(url, success: { (image) -> Void in
-            if let img = image {
-                self.image = img
-            }
-        })
+        if let image = UIImage(named: placeholderNamed) {
+            imageWithUrl(url: url, placeholder: image)
+        } else {
+            imageWithUrl(url: url)
+        }
     }
 }
